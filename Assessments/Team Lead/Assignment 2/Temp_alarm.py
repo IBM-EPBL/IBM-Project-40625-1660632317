5387fcc9b43514e9104effd136c88e3d15b67714
Venu Madhav <113613653+venumadhav2407@users.noreply.github.com>
2022-09-25 17:46:45 +0530

    Create Temp_alarm.py

---
+++ Assessments/Team Lead/Assignment 2/Temp_alarm.py
@@ -0,0 +1,16 @@
+# Team Lead(Bandla Venu Madhav)
+
+import random, time, winsound
+
+
+while True:
+    temp = random.randint(1, 100)
+    if temp > 60:
+        print("Alert Temparature is high!!")
+        winsound.PlaySound("beep-07a.wav", winsound.SND_FILENAME)
+    else:
+        print("Temparature is Normal")
+        
+    time.sleep(1)
+
+
