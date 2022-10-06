8e7bfed4c33a46f84bd048dc820bcdfc54650b12
Kumar A <113530946+kumarA4018@users.noreply.github.com>
2022-10-06 18:35:40 +0530

    Create LED Blinking pycode KumarA.py

---
+++ Assessments/Kumar/Assignment 3/LED Blinking pycode KumarA.py
@@ -0,0 +1,44 @@
+#IBM
+
+
+
+import RPI.GPIO as GPIO
+import time
+
+ledPin = 22
+def setup():
+    GPIO.setmode(GPIO.BOARD) 
+    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.setup(ledPin, GPIO.OUT)
+
+def loop():
+
+    while True:
+
+        print("LED on")
+
+        GPIO.output(ledPin, GPIO.HIGH)
+
+        time.sleep(1.0)
+
+        print("LED off")
+
+        GPIO.output(ledPin, GPIO.LOW)
+
+        time.sleep(1.0)
+
+def endprogram():
+
+    GPIO.output(ledPin, GPIO.LOW)
+
+    GPIO.cleanup()
+    
+if(name == 'main'):
+    setup()
+
+try:
+    loop()
+
+except KeyboardInterrupt:
+
+    endprogram()
