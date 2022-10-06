9b3b99961edb9fbb0541804775839d9cc6a152c7
Kumar A <113530946+kumarA4018@users.noreply.github.com>
2022-10-06 18:48:29 +0530

    Update Traffic Lights pycode KumarA .py

--- Assessments/Kumar/Assignment 3/Traffic Lights pycode KumarA .py
+++ Assessments/Kumar/Assignment 3/Traffic Lights pycode KumarA .py
@@ -1,3 +1,10 @@
+#IBM Assessment
+#Assignment - 3
+#Kumar A
+
+#Write python code for blinking LED and Traffic Lights for Raspberry pi. Only pyhton code is enough,no need to excute in raspberry pi.
+
+#Traffic Lights python code for Raspberry pi.
 from gpiozero import LED
 
 from time import sleep
