760e3834dd83b9e1d71e627da9dc54b54eed45aa
Venu Madhav <113613653+venumadhav2407@users.noreply.github.com>
2022-10-06 17:01:59 +0530

    Create Blink_LEB.py

---
+++ Assessments/Team Lead/Assignment 3/Blink_LEB.py
@@ -0,0 +1,26 @@
+# Bandla Venu Madhav (Team Lead)
+# 112819104007
+
+
+# Python code for blinking LED
+
+import machine
+import utime
+
+led _ red = machine. Pin(15, machine.Pin.OUT)
+led_yeltow = machine.Pin(14, machine.Pin.OUT)
+led_green = machine.Pin(13, machine. Pin. OUT)
+
+white True:
+    led_red.value(1)
+    utime.sleep(5)
+    led_yellow.value(1)
+    utime.sleep(2)
+    led red.value(0)
+    led_yellow.value(0)
+    led_green.value(1)
+    utime.sleep(5)
+    led_green.value(0)
+    led_yellow.value(l)
+    utime.sleep(5)
+    led_yellow.value(O)
