16e26e80f8ca360d415076d25e96c41477cab294
Kumar A <113530946+kumarA4018@users.noreply.github.com>
2022-10-07 11:09:02 +0530

    Update LED Blinking pycode KumarA.py

--- Assessments/Kumar/Assignment 3/LED Blinking pycode KumarA.py
+++ Assessments/Kumar/Assignment 3/LED Blinking pycode KumarA.py
@@ -1,6 +1,7 @@
 #IBM Assessment
 #Assignment - 3
 #Kumar A
+#Reg_No: 112819104018
 
 #Write python code for blinking LED and Traffic Lights for Raspberry pi. Only pyhton code is enough,no need to excute in raspberry pi.
 
