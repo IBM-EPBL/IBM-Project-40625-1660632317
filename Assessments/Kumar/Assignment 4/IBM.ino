e246de8221c61d90f458e8900a9bfbfef8fa8b3e
Kumar A <113530946+kumarA4018@users.noreply.github.com>
2022-11-02 14:04:17 +0530

    Update IBM.ino

--- Assessments/Kumar/Assignment 4/IBM.ino
+++ Assessments/Kumar/Assignment 4/IBM.ino
@@ -1,4 +1,16 @@
+/*Assignment 4:
 
+//KUMAR A
+
+//Reg_No:112819104018
+
+//Write code and connections in wokwi for the ultrasonic sensor.
+
+Whenever the distance is less than 100 cms send an "alert" to the IBM cloud and display in
+
+the device recent events.
+
+Upload document with wokwi share link and images of IBM cloud*/
 
 
 
