c3b746580192a82558d2612428316f84065a2891
Venu Madhav <113613653+venumadhav2407@users.noreply.github.com>
2022-11-03 19:37:27 +0530

    Update EPS32_assign.ino

--- Assessments/Team Lead/Assignment 4/EPS32_assign.ino
+++ Assessments/Team Lead/Assignment 4/EPS32_assign.ino
@@ -1,40 +1,129 @@
-// Bandla Venu Madhav (Team Lead)
+// 112819104007
 
-//Reg.No:  112819104007
+// Bandla Venu Madhav
+
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+
+void callback(char* subscribetopic, byte* payload, unsigned int payloadLength);
+//-------credentials of IBM Accounts------
+#define ORG "dakgpt"  // IBM ORGANITION ID
+#define DEVICE_TYPE "Esp32Projects"  // Device type mentioned in ibm watson IOT Platform
+#define DEVICE_ID "200105"  // Device ID mentioned in ibm watson IOT Platform
+#define TOKEN "wNXRmaEZ3i!@HcWyv6"  // Token
+String data3;
 
 
 
-// defines pins numbers
-const int trigPin = 2;
-const int echoPin = 5;
-// defines variables
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char publishTopic[] = "iot-2/evt/Data/fmt/json";
+char subscribetopic[] = "iot-2/cmd/test/fmt/String";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, callback, wifiClient);
+const int trigPin = 5;
+const int echoPin = 18;
+#define SOUND_SPEED 0.034
 long duration;
-int distance;
+float distance;
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600); // Starts the serial communication
+    Serial.begin(115200);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    wificonnect();
+    mqttconnect();
 }
 void loop() {
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * SOUND_SPEED / 2;
+    Serial.print("Distance (cm): ");
+    Serial.println(distance);
+    if (distance < 100) {
+        Serial.println("ALERT!!");
+        delay(1000);
+        PublishData(distance);
+        delay(1000);
+        if (!client.loop()) {
+            mqttconnect();
+        }
+    }
+    delay(1000);
+}
+void PublishData(float dist) {
+    mqttconnect();
 
-  if(distance <= 100){
-    Serial.println("Alert Distance is less than 100 cm");
-    
-  }
 
+    String payload = "{\"Distance\":";
+    payload += dist;
+    payload += ",\"ALERT!!\":" "\"Distance less than 100cms\"";
+    payload += "}";
+
+    Serial.print("Sending payload: ");
+    Serial.println(payload);
+
+
+    if (client.publish(publishTopic, (char*)payload.c_str())) {
+        Serial.println("Publish ok");
+    } else {
+        Serial.println("Publish failed");
+    }
+}
+
+void mqttconnect() {
+    if (!client.connected()) {
+        Serial.print("Reconnecting client to ");
+        Serial.println(server);
+        while (!!!client.connect(clientId, authMethod, token)) {
+            Serial.print(".");
+            delay(500);
+        }
+        initManagedDevice();
+        Serial.println();
+    }
+}
+void wificonnect() {
+    Serial.println();
+    Serial.print("Connecting to ");
+
+    WiFi.begin("Wokwi-GUEST", "", 6);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+void initManagedDevice() {
+    if (client.subscribe(subscribetopic)) {
+        Serial.println((subscribetopic));
+        Serial.println("subscribe to cmd OK");
+    } else {
+        Serial.println("subscribe to cmd FAILED");
+    }
+}
+
+void callback(char* subscribetopic, byte* payload, unsigned int payloadLength) {
+    Serial.print("callback invoked for topic: ");
+    Serial.println(subscribetopic);
+    for (int i = 0; i < payloadLength; i++) {
+        // Serial.print((char)payload[i]);
+        data3 += (char)payload[i];
+    }
+    Serial.println("data: " + data3);
+    data3 = "";
 }
